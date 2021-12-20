e47e820c9d555aa0b1ca54a7fe218220be8d5a09
dswaraj105 <57761161+dswaraj105@users.noreply.github.com>
2021-12-20 19:24:03 +0530

    added some features
    -loading spinner
    -message if there's no data
    -add fav count to navbar

--- app/app.js
+++ app/app.js
@@ -2,24 +2,27 @@ let app = angular.module("recepieApp", [
   "ngRoute",
   "recepieModule",
   "searchRecepieModule",
-  "favourate"
+  "favourate",
+  "ngCookies"
 ]);
-
 
 app.config([
   // Routing Configration
   "$routeProvider",
   function ($routeProvider) {
     $routeProvider
-      .when("/", {                                      // Route for main Page
+      .when("/", {
+        // Route for main Page
         templateUrl: "views/searchRecepie.html",
         controller: "searchRecepieController",
       })
-      .when("/resecipe/:id", {                          // Route for details Page
+      .when("/resecipe/:id", {
+        // Route for details Page
         templateUrl: "views/recepie.html",
         controller: "recepieController",
       })
-      .when("/favourates", {                          // Route for details Page
+      .when("/favourates", {
+        // Route for details Page
         templateUrl: "views/favourates.html",
         controller: "favourateController",
       })
@@ -29,89 +32,14 @@ app.config([
   },
 ]);
 
+app.controller("favourateController", ["$scope", "$cookies", function ($scope, $cookies) {
+  function InitPage() {
+    var data = $cookies.getObject("foods");
+    if(data){
+      // console.log("fav count - ", data.length);
+      $scope.favCount = data.length;
+    }
+  }
 
-
-// app.service("fetchData", [
-//   "$http",
-//   function ($http) {
-//     // fetching the list of food names for search keyword
-//     this.fetchNames = function (searchkey, successResponse) {
-//       $http
-//         .get(
-//           `https://api.edamam.com/api/recipes/v2?type=public&q=${searchkey}&from=20&to=40&app_id=a6561902&app_key=499f593c405f9c6151b2a0abd9da5360`
-//         )
-//         .then(successResponse, function (err) {
-//           console.log(err);
-//         });
-//     };
-
-//     // fetching a single food
-//     // ......
-//   },
-// ]);
-
-
-// Controller for search Page
-// app.controller("c1", [
-//   "$scope",
-//   "fetchData",
-//   "$http",
-//   function ($scope, fetchData, $http) {
-//     $scope.foodItems = [];
-
-//     // function to handle the form submission to fetch food names
-//     $scope.getFoods = function (searchKey) {
-//       // Checking if the search field is empty and if so do nothing
-//       if (searchKey === "") {
-//         return;
-//       }
-
-//       // calling the service to fetch data from the server
-//       var resData = fetchData.fetchNames(searchKey, handleSuccessResponse);
-
-//       console.log("in controller", resData);
-
-//       // Function to handle the success response from server
-//       function handleSuccessResponse(res) {
-//         console.log(res);
-
-//         $scope.foodItems = res.data;
-//         // console.log(foodItems.hits);
-
-//         return res;
-//       }
-
-//       // Clearing the input after successful response
-//       $scope.searchKey = "";
-//       console.log("Search foods for - ", searchKey);
-//     };
-
-//     $scope.getFoodsByKey = function (searchKey) {
-//       // Checking if the search field is empty and if so do nothing
-//       if (searchKey === "") {
-//         return;
-//       }
-
-//       $http
-//         .get(
-//           `https://api.edamam.com/api/recipes/v2?type=public&q=${searchKey}&from=20&to=40&app_id=a6561902&app_key=499f593c405f9c6151b2a0abd9da5360`
-//         )
-//         .then(function(res) {
-//           console.log("Got Response");
-
-//           var foodItems = res.data.hits;
-//           console.log("2 - ", foodItems);
-
-//           $scope.foodItems = foodItems;
-
-//         }, function (err) {
-//           console.log(err);
-//         });
-//     };
-//   },
-// ]);
-
-// Controller for the details page
-// app.controller("c2", function ($scope) {
-//   $scope.msg = "C2";
-// });
+  InitPage();
+}]);
--- app/searchRecepie/searchRecepie.js
+++ app/searchRecepie/searchRecepie.js
@@ -1,6 +1,5 @@
 let searchRecepie = angular.module("searchRecepieModule", []);
 
-
 searchRecepie.service("fetchData", [
   "$http",
   function ($http) {
@@ -27,6 +26,16 @@ searchRecepie.controller("searchRecepieController", [
   "$http",
   "$location",
   function ($scope, fetchData, $http, $location) {
+    // function InitPage() {
+    //   var data = $cookies.getObject("foods");
+    //   if(data){
+    //     console.log("fav count - ", data.length);
+    //     $scope.favCount = data.length;
+    //   }
+    // }
+
+    // InitPage();
+
     // To store all the food items for search
     $scope.foodItems = [];
 
@@ -37,12 +46,21 @@ searchRecepie.controller("searchRecepieController", [
     var page = 1;
     var lastSearchKey = "";
 
+    // To control the visibility of the loading spinner
+    $scope.showSpinner = false;
+
+    // To store the message if we dont get any data for the search
+    $scope.badSearchMsg = false;
+
     // function to handle the form submission to fetch food names
     $scope.getFoods = function (searchKey) {
       // Checking if the search field is empty and if so do nothing
       if (searchKey === "") {
         return;
       }
+
+      // displaying the loading spinner
+      // $scope.showSpinner = true;
 
       // calling the service to fetch data from the server
       var resData = fetchData.fetchNames(searchKey, handleSuccessResponse);
@@ -79,13 +97,27 @@ searchRecepie.controller("searchRecepieController", [
           function (res) {
             console.log("Got Response", res);
 
+            if (res.data.count === 0) {
+              console.log("count - ", res.data.count);
+              $scope.badSearchMsg = `Theres no result for ${searchKey}`;
+              $scope.showFetchMore = false;
+              $scope.showSpinner = false;
+              return;
+            }
+
             var foodItems = res.data.hits;
             // console.log("2 - ", foodItems);
+
+            // Since we have the response we hide the loading spinner
+            $scope.showSpinner = false;
 
             // Setting data to the model for displaying items
             $scope.foodItems = foodItems;
           },
           function (err) {
+            // Even in error, response we hide the loading spinner
+            $scope.showSpinner = false;
+
             console.log(err);
           }
         );
@@ -93,12 +125,15 @@ searchRecepie.controller("searchRecepieController", [
       // Clearing the input after successful response
       $scope.searchKey = "";
 
+      // Display the loading spinner
+      $scope.showSpinner = true;
+
       // resetting page number and search key
       page = 1;
       lastSearchKey = searchKey;
 
       // Used to see if there are miore items available
-      if($scope.foodItems.length < 12){
+      if ($scope.foodItems.length < 12) {
         $scope.showFetchMore = true;
       } else {
         $scope.showFetchMore = false;
@@ -106,7 +141,9 @@ searchRecepie.controller("searchRecepieController", [
     };
 
     // Getting next Page if exists
-    $scope.getNextPage = function(){
+    $scope.getNextPage = function () {
+      // Displaying the loading spinner
+      $scope.showSpinner = true;
 
       // Fixing the start and end values for getting further pages
       var low = page * 12;
@@ -124,36 +161,47 @@ searchRecepie.controller("searchRecepieController", [
             // new food details
             var newfoodItems = res.data.hits;
 
+            if (newfoodItems.length < 12) {
+              console.log("End of search");
+              $scope.badSearchMsg = `Sorry, We are at the end of the page`;
+              $scope.showFetchMore = false;
+              $scope.showSpinner = false;
+              return;
+            }
+
             // existing details
             var availableItems = $scope.foodItems;
 
             // Concatinating all the available data too display
             var all = availableItems.concat(newfoodItems);
 
+            // Since we have the response we hide the loading spinner
+            $scope.showSpinner = false;
+
             $scope.foodItems = all;
-            console.log("final", all);
+            console.log("final", res);
           },
           function (err) {
+            // Even in error response we hide the loading spinner
+            $scope.showSpinner = false;
 
             // Catching the errors
             console.log(err);
           }
         );
+    };
 
-    }
-
-    // Function to take to the details page 
-    $scope.loadDetailsPage = function(url){
-
+    // Function to take to the details page
+    $scope.loadDetailsPage = function (url) {
       console.log(url);
 
       var parts = url.split("/");
-      var id = parts[parts.length - 1].split('_')[1];
+      var id = parts[parts.length - 1].split("_")[1];
 
       console.log("Loading details page - ", id);
 
       // Changing the location of the page
       $location.path(`/resecipe/${id}`);
-    }
+    };
   },
 ]);
